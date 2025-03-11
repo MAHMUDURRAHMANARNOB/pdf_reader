c80e118f79144cf2e1cf83407d24cfafe16e1e9e
MAHMUDURRAHMANARNOB <mahmudur.rahman2023@gmail.com>
2025-03-11 14:50:01 +0600

    feat: PDF reader worked
    > Pdf reader worked with PDFX

--- lib/screens/PdfViewerScreen.dart
+++ lib/screens/PdfViewerScreen.dart
@@ -1,20 +1,36 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:pdfx/pdfx.dart';
 
-class PdfViewerScreen extends StatelessWidget {
+class PdfViewerScreen extends StatefulWidget {
   final String pdfPath;
+  const PdfViewerScreen({Key? key, required this.pdfPath}) : super(key: key);
 
-  const PdfViewerScreen({super.key, required this.pdfPath});
+  @override
+  _PdfViewerScreenState createState() => _PdfViewerScreenState();
+}
+
+class _PdfViewerScreenState extends State<PdfViewerScreen> {
+  late PdfController pdfController;
+
+  @override
+  void initState() {
+    super.initState();
+    pdfController = PdfController(
+      document: PdfDocument.openFile(widget.pdfPath),
+    );
+  }
+
+  @override
+  void dispose() {
+    pdfController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("PDF Viewer"),
-      ),
-      body: SfPdfViewer.file(File(pdfPath)),
+      appBar: AppBar(title: const Text("PDF Viewer")),
+      body: PdfView(controller: pdfController),
     );
   }
 }
